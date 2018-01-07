807a84ef2a16a01e3304fe087c51ce912eb33504
Adam Harrison <ad@mharrison.us>
2018-01-07 13:25:51 -0500

    logger fixes

--- src/main/java/com/ulfric/plugin/platform/logging/LoggerBinding.java
+++ src/main/java/com/ulfric/plugin/platform/logging/LoggerBinding.java
@@ -8,12 +8,11 @@ import com.ulfric.dragoon.ObjectFactory;
 import com.ulfric.dragoon.Parameters;
 import com.ulfric.dragoon.extension.inject.Inject;
 import com.ulfric.dragoon.logging.DefaultLoggerBinding;
-import com.ulfric.dragoon.logging.Log;
 
 public class LoggerBinding implements Function<Parameters, Logger> {
 
-	@Inject
-	private Log formerLogger;
+	@Inject(optional = true)
+	private Logger formerLogger;
 
 	@Inject
 	private ObjectFactory factory;
@@ -26,7 +25,9 @@ public class LoggerBinding implements Function<Parameters, Logger> {
 			return plugin.getLogger();
 		}
 
-		formerLogger.log(Level.INFO, "Falling back to default logger binding for {0}", parameters.getQualifier());
+		if (formerLogger != null) {
+			formerLogger.log(Level.INFO, "Falling back to default logger binding for {0}", parameters.getQualifier());
+		}
 
 		return DefaultLoggerBinding.INSTANCE.apply(parameters);
 	}
